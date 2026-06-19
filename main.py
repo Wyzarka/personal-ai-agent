45f9a7deda8abcd3b617e3c991f067130923ffcb
u201cYour <u201cyour.email@example.comu201d>
2026-06-19 17:35:39 -0500

    Second commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
@@ -9,9 +10,12 @@ if not api_key:
     raise RuntimeError("API key missing")
 client = genai.Client(api_key=api_key)
 
+parser = argparse.ArgumentParser(description="Chatbot prompt")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    contents=args.user_prompt,
 )
 prompt_tokens = response.usage_metadata.prompt_token_count
 response_tokens = response.usage_metadata.candidates_token_count
