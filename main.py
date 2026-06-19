0cd6a8e99292dc445ae0d78b8b96153bfd190eb6
u201cYour <u201cyour.email@example.comu201d>
2026-06-18 20:47:26 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if not api_key:
+    raise RuntimeError("API key missing")
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+)
+prompt_tokens = response.usage_metadata.prompt_token_count
+response_tokens = response.usage_metadata.candidates_token_count
+
+if not prompt_tokens or not response_tokens:
+    raise RuntimeError("API request failed")
+print(f"Prompt tokens: {prompt_tokens}")
+print(f"Response tokens: {response_tokens}")
+print(response.text)
+
+
+def main():
+    print("Hello from personal-ai-agent!")
+
+
+if __name__ == "__main__":
+    main()
