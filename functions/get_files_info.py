c8c75a9c2a336f77d722dcbff9b00216310e5d38
u201cYour <u201cyour.email@example.comu201d>
2026-06-24 00:33:50 -0500

    Fifth commit

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,24 +2,21 @@ import os
 
 
 def get_files_info(working_directory: str, directory: str):
-    abs_working_directory = os.path.abspath(working_directory)
-    target_dir = os.path.normpath(os.path.join(abs_working_directory, directory))
-    is_valid_dir = (
-        os.path.commonpath([abs_working_directory, target_dir]) == abs_working_directory
-    )
     try:
-        if not is_valid_dir:
-            print(
-                f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-            )
-            return
+        abs_working_dir = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(abs_working_dir, directory))
+        if os.path.commonpath([abs_working_dir, target_dir]) != abs_working_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if not os.path.isdir(target_dir):
-            print(f'Error: "{directory}" is not a directory')
-            return
-        else:
-            print(f'Success: "{directory}" is within the working directory')
-            return
-
+            return f'Error: "{directory}" is not a directory'
+        files_info: list[str] = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
     except Exception as e:
-        print(f"Error: {e}")
-        return
+        return f"Error listing files: {e}"
